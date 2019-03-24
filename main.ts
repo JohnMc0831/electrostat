382322dddd94fc7152951b340bfe8abc4a28fc3b
John McConnell <johnmc0831@gmail.com>
2019-03-24 14:56:10 -0400

    added winston for logging.  Also hid mainWindow and added tray icon with context menu.

--- main.ts
+++ main.ts
@@ -1,37 +1,36 @@
-import { app, BrowserWindow, screen } from 'electron';
+import { app, BrowserWindow, screen, Tray, Menu, nativeImage, shell, ipcMain, webContents } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
-import * as signalR from '@aspnet/signalr';
 import { INTERNAL_BROWSER_DYNAMIC_PLATFORM_PROVIDERS } from '@angular/platform-browser-dynamic/src/platform_providers';
-import {catService, catProd } from './src/app/models/logging';
-let win, serve;
+import * as winston from 'winston';
+
+let win: BrowserWindow;
+let contents: webContents;
+let serve;
+const tempDir = app.getPath('temp');
+const logPath = path.join(tempDir, './electoStat.log');
 const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
-
-
-function loadUp() {
-  initSignalR();
-  createWindow();
-}
-
-function initSignalR() {
-  catProd.info('Initializing signalR client');
-  const connection = new signalR.HubConnectionBuilder()
-    .withUrl('https://stat.uvmhealth.org/alertHub')
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
-connection.start().then(function () {
-    catProd.info('signalR sub-system is now connected...');
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.simple(),
+  transports: [
+    new winston.transports.File({ filename: logPath }),
+    new winston.transports.Console({
+      format:
+        winston.format.combine(
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          winston.format.printf(info => `${info.timestamp}: ${info.level} - ${info.message}`)
+        ),
+    })
+  ]
 });
-}
 
 function createWindow() {
-
   const electronScreen = screen;
   const size = electronScreen.getPrimaryDisplay().workAreaSize;
-  // tslint:disable-next-line:quotemark
-  catProd.info("creating main window...");
+  logger.info('Creating electroStat Main Window...');
   // Create the browser window.
   win = new BrowserWindow({
     x: (size.width / 2) - 400,
@@ -41,10 +40,11 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
     },
-    icon: __dirname + '/dist/favicon.png'
+    icon: __dirname + '/dist/favicon.png',
+    show: false
   });
-
-  catProd.info(`icon file: ${win.icon}`);
+  // use this object for ipc messaging
+  contents = win.webContents;
 
   if (serve) {
     require('electron-reload')(__dirname, {
@@ -58,11 +58,66 @@ function createWindow() {
       slashes: true
     }));
   }
+  // JRM edits here
+  win.setResizable(false); // no window resizing for you!
+  logger.info('Sending showLastALert to ipcRenderer');
+  contents.send('showLastAlert');
+  // tray icon and context menu
+  let tray = null;
+  const iconPath = path.join(__dirname, './dist/favicon.png');
+  let trayIcon = nativeImage.createFromPath(iconPath);
+  trayIcon = trayIcon.resize({width: 16, height: 16});
+  logger.info(`trayIcon path: ${iconPath}`);
+  tray = new Tray(trayIcon);
 
-  if (serve) {
-    win.webContents.openDevTools();
-  }
-
+  const menuTemplate: Electron.MenuItemConstructorOptions[] = [
+    {
+      label: 'See Last Alert',
+      type: 'normal',
+      click(item) {
+       seeLastAlert();
+      }
+    },
+    {
+      label: 'Show Past Alerts',
+      type: 'normal',
+      click(item) {
+        showPastAlerts();
+      }
+    },
+    {
+      label: 'View Log File',
+      type: 'normal',
+      click(item) {
+        viewLogFile();
+      }
+    },
+    { type: 'separator'},
+    {
+      label: 'Play Sound?',
+      type: 'checkbox',
+      checked: true,
+      click(item) {
+        togglePlaySound();
+      }
+    },
+    {
+      label: 'Show ALL alerts?',
+      type: 'checkbox',
+      checked: false,
+      click(item) {
+        toggleShowAll();
+      }
+    },
+    { type: 'separator' },
+    {
+      label: 'Exit',
+      role: 'quit'
+    },
+  ];
+  const contextMenu = Menu.buildFromTemplate(menuTemplate);
+  tray.setToolTip('Stat!');
+  tray.setContextMenu(contextMenu);
   // Emitted when the window is closed.
   win.on('closed', () => {
     // Dereference the window object, usually you would store window
@@ -70,15 +125,40 @@ function createWindow() {
     // when you should delete the corresponding element.
     win = null;
   });
-
 }
+
+function viewLogFile() {
+  logger.info('viewLogFile was clicked!');
+}
+
+function showPastAlerts() {
+  logger.info('showPastAlerts was clicked!');
+  shell.openExternal('https://stat.uvmhealth.org/home/alerts');
+}
+
+function seeLastAlert() {
+  // TODO: pass this from ipcMain to ipcRenderer
+  logger.info('See Last Alert menuItem was clicked!');
+}
+
+function togglePlaySound() {
+  logger.info('togglePlaySound() was clicked!');
+}
+
+function toggleShowAll() {
+  logger.info('toggleShowAll() was clicked!');
+}
+
+ipcMain.on('alertReceived', (event, arg) => {
+  logger.info('alertReceived call from ipcMain!');
+});
 
 try {
 
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
-  app.on('ready', loadUp);
+  app.on('ready', createWindow);
 
   // Quit when all windows are closed.
   app.on('window-all-closed', () => {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,11 @@
+import { app, BrowserWindow, remote, ipcRenderer } from 'electron';
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import * as moment from 'moment';
 import { StatAlert } from '../../models/models';
-import {catService, catProd } from '../../models/logging';
+import * as winston from 'winston';
+import * as signalR from '@aspnet/signalr';
+import * as path from 'path';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +13,66 @@ import {catService, catProd } from '../../models/logging';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public logger: winston.Logger;
+  constructor() {
+    const tempDir = app.getPath('temp');
+    const logPath = path.join(tempDir, 'statClient.log');
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.simple(),
+      transports: [
+        new winston.transports.File({ filename: logPath }),
+        new winston.transports.Console({
+          format:
+            winston.format.combine(
+              winston.format.colorize(),
+              winston.format.timestamp(),
+              winston.format.printf(info => `${info.timestamp}: ${info.level} - ${info.message}`)
+            ),
+        })
+      ]
+    });
+   }
   public alert: StatAlert = new StatAlert();
+  renderer: any = ipcRenderer;
 
   ngOnInit() {
-    catProd.info('running inside ngOnInit()');
-    catProd.info('Generating sample alert.');
-    this.alert.title = 'Test Alert!';
-    this.alert.narrative = '<p>Test alert!</p>';
-    this.alert.alertTime = moment().format('MM/DD/YYYY hh:mm:ss A');
+    this.initSignalR();
+
+    this.renderer.on('showLastAlert', function(event, data) {
+      this.logger.info('ipc message to show last alert received by renderer');
+    });
   }
 
+  initSignalR() {
+    this.logger.info('Initializing signalR client');
+    const connection = new signalR.HubConnectionBuilder()
+      .withUrl('https://stat.uvmhealth.org/alertHub')
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    connection.start().then(function () {
+      this.logger.info('signalR sub-system is now connected...');
+    }).catch((err) => {
+      this.logger.error(`An exception occurred starting the signalR client: ${err}`, err);
+    });
+
+    // start listening for alerts
+    connection.on('broadcastAlert', (alert) => {
+      this.displayAlert(alert);
+    });
+  }
+
+  displayAlert(alert: StatAlert) {
+    this.logger.info(`Stat alert received at ${moment().format('MM/DD/YYYY hh:mm:ss a')}.`);
+    this.alert = alert;
+    $('#alertWindow').removeClass('gradientBackground').addClass('emergentAlert');
+    const win = remote.getCurrentWindow();
+    win.show();
+  }
+
+  seeLastAlert() {
+    // TODO: pass this from ipcMain to ipcRenderer
+    this.logger.info('See Last Alert menuItem was clicked!');
+  }
 }
