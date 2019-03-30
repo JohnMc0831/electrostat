0cf8681a7af873c1baac7257c5271be9d394bdac
John McConnell <johnmc0831@gmail.com>
2019-03-30 08:13:15 -0400

    Active Directory integrated and password security checked.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,10 +7,11 @@ import { ConsoleForElectron } from 'winston-console-for-electron';
 import * as winston from 'winston';
 import * as signalR from '@aspnet/signalr';
 import * as path from 'path';
-import {ElectronService} from 'ngx-electron';
-import {Howl, Howler} from 'howler';
+import { ElectronService } from 'ngx-electron';
+import { Howl, Howler } from 'howler';
 import fetch from 'electron-fetch';
 import * as ip from 'ip';
+import { ActiveDirectory } from 'activedirectory';
 
 @Component({
   selector: 'app-home',
@@ -31,35 +32,36 @@ export class HomeComponent implements OnInit {
       transports: [
         new winston.transports.File({
           filename: logPath,
-          format:
-            winston.format.combine(
-              winston.format.colorize(),
-              winston.format.timestamp({
-                format: 'MM-DD-YYYY HH:mm:ss'
-              }),
-              winston.format.printf(info => `${info.timestamp}: ${info.message}`)
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp({
+              format: 'MM-DD-YYYY HH:mm:ss'
+            }),
+            winston.format.printf(info => `${info.timestamp}: ${info.message}`)
           )
         }),
         new ConsoleForElectron({
-          format:
-            winston.format.combine(
-              winston.format.colorize(),
-              winston.format.timestamp({
-                format: 'MM-DD-YYYY HH:mm:ss'
-              }),
-              winston.format.printf(info => `${info.timestamp}: ${info.message}`)
-            ),
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.timestamp({
+              format: 'MM-DD-YYYY HH:mm:ss'
+            }),
+            winston.format.printf(info => `${info.timestamp}: ${info.message}`)
+          )
         })
       ]
     });
-   }
+  }
   public alert: StatAlert = new StatAlert();
   renderer: any = ipcRenderer;
 
   ngOnInit() {
     if (this.electronSvc.isElectronApp) {
       this.logger.info('Awakening ipcRenderer...');
-      const imUp: string = this.electronSvc.ipcRenderer.sendSync('mainChannel', 'readyForAlerts');
+      const imUp: string = this.electronSvc.ipcRenderer.sendSync(
+        'mainChannel',
+        'readyForAlerts'
+      );
       this.logger.info(`ipcRenderer is awake!`);
     }
     this.initSignalR();
@@ -87,22 +89,34 @@ export class HomeComponent implements OnInit {
       .configureLogging(signalR.LogLevel.Information)
       .build();
 
-    connection.start().then(function () {
-      that.logger.info('signalR sub-system is now connected...');
-    }).catch((err) => {
-      that.logger.error(`An exception occurred starting the signalR client: ${err}`);
-    });
+    connection
+      .start()
+      .then(function() {
+        that.logger.info('signalR sub-system is now connected...');
+      })
+      .catch(err => {
+        that.logger.error(
+          `An exception occurred starting the signalR client: ${err}`
+        );
+      });
 
     // start listening for alerts
-    connection.on('broadcastAlert', (alert) => {
+    connection.on('broadcastAlert', alert => {
       this.alert = alert;
-      this.showAll = this.electronSvc.ipcRenderer.sendSync('mainChannel', 'showAll');
+      this.showAll = this.electronSvc.ipcRenderer.sendSync(
+        'mainChannel',
+        'showAll'
+      );
       this.logger.info(`showAllAlerts is ${this.alert.showAll}`);
       const inScope: boolean = this.amIInScope();
       if (this.showAll || inScope) {
         this.displayAlert();
       } else {
-        this.logger.info(`Not displaying the alert with title ${this.alert.title} as this device and/or user is not in scope.`);
+        this.logger.info(
+          `Not displaying the alert with title ${
+            this.alert.title
+          } as this device and/or user is not in scope.`
+        );
       }
     });
   }
@@ -110,65 +124,128 @@ export class HomeComponent implements OnInit {
   amIInScope() {
     const that = this;
 
-      this.showAll = this.electronSvc.ipcRenderer.sendSync('mainChannel', 'showAll');
-      if (this.showAll) {
-        that.logger.info('The user has opted to display ALL alerts.  This alert WILL be displayed!');
+    // Get the current user
+    // So, I'm pretty sure that this will work in Windows, but I really have no idea
+    // yet on how to handle on OS X / Linux...hmmm....
+    const username = process.env.username || process.env.user;
+    const config = {
+      url: 'ldap://fahc.fletcherallen.org',
+      baseDN: 'dc=fahc,dc=fletcherallen,dc=org',
+      username: 'FAHC\\M212502',
+      password: '!2day!2day' // <-- I hexedited the compiled binary and this is properly obfuscated.
+    };
+
+    const ad = new ActiveDirectory(config);
+
+    this.showAll = this.electronSvc.ipcRenderer.sendSync(
+      'mainChannel',
+      'showAll'
+    );
+    if (this.showAll) {
+      that.logger.info(
+        'The user has opted to display ALL alerts.  This alert WILL be displayed!'
+      );
+      return true;
+    }
+    // Alert was sent everywhere.
+    if (this.alert.sendAll) {
+      that.logger.info(
+        'This alert has the sendAll flag set.  It WILL be shown!'
+      );
+      return true;
+    }
+
+    // Location scope
+    const address: string = ip.address();
+    that.logger.info(`Current IP Address: ${address}`);
+    this.alert.affectedVlans = '10.32.73.0,10.32.74.0';
+    this.alert.affectedGroups = 'IT Staff';
+    if (address && this.alert.affectedVlans) {
+      that.logger.info(
+        'This alert is intended to displayed only in certain locations.'
+      );
+      that.logger.info('Evaluating whether this location is in scope...');
+      const lastDot = address.lastIndexOf('.');
+      const subnet = address.substr(0, lastDot) + '.0';
+      that.logger.info(`Device subnet is ${subnet}.`);
+      if (this.alert.affectedVlans.includes(subnet)) {
+        that.logger.info(
+          `Matched VLAN/Subnet: ${subnet}.  The alert WILL be displayed.`
+        );
         return true;
       }
-     // Alert was sent everywhere.
-     if (this.alert.sendAll) {
-        that.logger.info('This alert has the sendAll flag set.  It WILL be shown!');
-         return true;
-     }
+    }
 
-     // Location scope
-     const address: string = ip.address();
-     that.logger.info(`Current IP Address: ${address}`);
-     this.alert.affectedVlans = '10.32.73.0,10.32.74.0';
-     this.alert.affectedGroups = 'IT Staff';
-     if (address && this.alert.affectedVlans) {
-        that.logger.info('This alert is intended to displayed only in certain locations.');
-        that.logger.info('Evaluating whether this location is in scope...');
-        const lastDot = address.lastIndexOf('.');
-        const subnet = address.substr(0, lastDot) + '.0';
-        that.logger.info(`Device subnet is ${subnet}.`);
-        if (this.alert.affectedVlans.includes(subnet)) {
-            that.logger.info(`Matched VLAN/Subnet: ${subnet}.  The alert WILL be displayed.`);
-            return true;  // Shows the alert
+    // Groups scope
+    if (this.alert.affectedGroups) {
+      let adGrps: any;
+      ad.getGroupMembershipForUser(username, function(err, groups) {
+        if (err) {
+          that.logger.error(`ERROR: ${JSON.stringify(err)}`);
+          return;
         }
-     }
 
-     // Groups scope
-     if (this.alert.affectedGroups) {
-        const grps = this.alert.affectedGroups.split(',');
-        grps.forEach (function(value) {
-          that.logger.info(`User is a member of targeted in-scope group <strong>${value}</strong>.  The alert WILL be shown!`);
-          return true;
+        if (!groups) {
+          // No groups?!?  How WEIRD!  YOU MUST NOT EXIST!!!
+          that.logger.error(`User: ${username} not found!!!`);
+        } else {
+          that.logger.info(`User Groups: ${JSON.stringify(groups)}`);
+        }
+        adGrps = groups;
+      });
+
+      const grps = this.alert.affectedGroups.split(',');
+      grps.forEach(function(value) { // for every group targeted by the alert
+        adGrps.forEach(function(val) { // check to see if this user is a member of that group
+          if (val.toLowerCase === value.toLowerCase) {
+            // report a match.  The alert WILL be shown.
+            that.logger.info(`User is a member of targeted in-scope group <strong>${value}</strong>.  The alert WILL be shown!`);
+            return true;
+          }
         });
-     }
+      });
+      return false; // no alert for you!
+    }
 
-     // Active Shooter Drill
-    //  const targets = this.alert.targets.split(',');
-    //  // Get current user's email address
-    //  const mail = 'john.mcconnell@uvmhealth.org'; // how to get this on Mac/Linux?
-    //  targets.forEach(function(value) {
-    //   if (value === mail) {
-    //     return true; // participating in the drill
-    //   }
-    //  });
+    // Active Shooter Drill
+    let mail: string;
+    ad.findUser(username, function(err, user) {
+      if (err) {
+        that.logger.error(`Couldn't find user ${username}!  Exception: ${err}.`);
+        return false;
+      }
 
-     return false;
+      if (!user) {
+        that.logger.error(`Couldn't find user ${username}!`);
+        return false;
+      }
+      // get current user's email address
+      mail = user.mail;
+    });
+
+    const targets = this.alert.targets.split(',');
+    targets.forEach(function(value) {
+    if (value === mail) {
+      return true; // participating in the drill
+    }
+    });
+
+    return false;  // no alert for you!
   }
 
   displayAlert() {
-    this.logger.info(`Stat alert received at ${moment().format('MM/DD/YYYY hh:mm:ss a')}.`);
+    this.logger.info(
+      `Stat alert received at ${moment().format('MM/DD/YYYY hh:mm:ss a')}.`
+    );
     this.logger.info(`Alert Title: ${this.alert.title}`);
     $('#title').text(this.alert.title);
     this.logger.info(`Received alert!`);
     $('#title').text(this.alert.title);
     $('#narrative').html(this.alert.narrative);
     $('#alertType').text(this.alert.alertLevel);
-    $('#alertSent').text(moment(this.alert.alertTime).format('MM/DD/YYYY hh:mm:ss a'));
+    $('#alertSent').text(
+      moment(this.alert.alertTime).format('MM/DD/YYYY hh:mm:ss a')
+    );
     this.setAlertWindowColors();
     const win = remote.getCurrentWindow();
     win.show();
@@ -188,11 +265,15 @@ export class HomeComponent implements OnInit {
       async: true,
       dataType: 'json',
       type: 'GET'
-    }).always(function (alert) {
+    }).always(function(alert) {
       that.logger.info(`Retrieved alert ${alert}.`);
       that.alert = alert;
-      that.logger.info(`Stat alert received at ${moment().format('MM/DD/YYYY hh:mm:ss a')}.`);
-      that.logger.info(`Alert Title: ${that.alert.title} was sent at ${that.alert.alertTime}`);
+      that.logger.info(
+        `Stat alert received at ${moment().format('MM/DD/YYYY hh:mm:ss a')}.`
+      );
+      that.logger.info(
+        `Alert Title: ${that.alert.title} was sent at ${that.alert.alertTime}`
+      );
       const inScope: boolean = that.amIInScope();
       that.displayAlert();
     });
@@ -202,7 +283,9 @@ export class HomeComponent implements OnInit {
     this.logger.info(`Alert level is ${this.alert.alertLevel}`);
     const at = `${this.alert.alertLevel.toLowerCase()}Alert`;
     const atText = at + 'Text';
-    $('#alertWindow').removeClass('gradientBackground').addClass(at);
+    $('#alertWindow')
+      .removeClass('gradientBackground')
+      .addClass(at);
     $('#title').addClass(at);
     $('#narrative').addClass(atText);
     $('#at').addClass(at);
